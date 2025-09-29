e3536ef98d440f02e7252b44a427d0ad8c304504
rahul <rahulprince0786@gmail.com>
2025-09-29 19:30:34 +0530

    refercing variable

---
+++ referenceVariable.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+using namespace std;
+void changeA(int &num){
+    num = 56;
+    cout<<num;
+}
+int main(){
+    //reference variable -- it is variable that point the same memory location with different name
+    int a = 5;
+    cout<<" value of a :"<<a;
+    cout<<"value of num is :";
+    changeA(a);
+    cout<<" value of a :"<<a;
+
+    return 0;
+}
